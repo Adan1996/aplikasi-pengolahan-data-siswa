ce0d2fb7331bef673aa238dc5c7a83b363d06482
Adan1996 <syahdanmasyhuri@gmail.com>
2019-11-29 19:36:37 +0700

    menambahkan file bootstrap ke dalam html

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>
-        @yield('title')
-    </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('bootstrap-4.3.1/css/bootstrap.min.css') }}">
+
+    <title>@yield('title')</title>
 </head>
 
 <body>
@@ -15,8 +17,17 @@
 
 
     <div id="footer">
-        <p>&copy; 2019 Situs Sekolah</p>
+        <p>&copy; 2019 | sekolahku app</p>
     </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ asset('js/jquery-3.4.1.min.js') }}">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('bootstrap-4.3.1/js/bootstrap.min.js') }}">
+    </script>
 </body>
 
 </html>
