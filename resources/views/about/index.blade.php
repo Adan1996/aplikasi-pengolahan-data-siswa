f0855dea8f4bfcde7cd6dd8bf69c6828adcdb9a8
Adan1996 <syahdanmasyhuri@gmail.com>
2019-11-29 18:22:12 +0700

    menambahkan halaman homepage dan about

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,4 @@
+<div class="about">
+    <h2>About Us</h2>
+    <p>Ini adalah situs Sekolah</p>
+</div>
---
+++ resources/views/homepage/index.blade.php
@@ -0,0 +1,4 @@
+<div class="about">
+    <h2>Homepage</h2>
+    <p>Selamat Datang di Website Kami</p>
+</div>
